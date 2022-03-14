05e6bf96d999072a3e3011dae3159447afb045b8
jsantis <san21153@uvg.edu.gt>
2022-03-13 20:35:12 -0600

    Creación de función procesos

--- HDT5.py
+++ HDT5.py
@@ -8,6 +8,35 @@ import simpy
 import random
 
 
+#Función para realizar procesos
+def procesos(env, espera_inicio, nombre, cantidad_ram, cantidad_instrucciones, instrucciones_ciclo, operaciones_ciclo, ram_inicial, nucleos):
+    estado = False #not terminated
+
+    yield env.timeout(espera_inicio)
+    start = env.now
+
+    print("%s proceso en cola [NEW]. Tiempo: %d .  RAM requerida: %d .  RAM disponible: %d" % (nombre, env.now, cantidad_ram, ram_inicial.level))
+    yield ram_inicial.get(cantidad_ram)
+    print("%s proces en cola [READY] en tiempo %d. Cantidad de instrucciones pendientes %d" % (nombre, env.now, cantidad_instrucciones))
+
+
+    while cantidad_instrucciones > 0:
+        with nucleos.request() as req:
+            yield req
+            cantidad_instrucciones -= instrucciones_ciclo
+            yield env.timeout(operaciones_ciclo) #ciclos cada operacion
+            print("%s proces en cola [READY] en tiempo %d. Cantidad de instrucciones pendientes %d" % (nombre, env.now, cantidad_instrucciones))
+
+        if cantidad_instrucciones > 0:
+            evento_random = random.randint(1, 2)
+
+
+    yield ram_inicial.put(cantidad_ram)
+    global tiempo_total
+    tiempo_total += env.now - start
+    print("%s proceso [TERMINATED] en tiempo %d. Cantidad de RAM devuelta: %d. Cantidad de memoria disponible %d" % (nombre, env.now, cantidad_ram, ram_inicial.level))
+
+
 # setup del programa
 slots_ram = 100 #slots de RAM
 cant_procesos = 25 #cantidad de procesos a realizar  (optimizable)
